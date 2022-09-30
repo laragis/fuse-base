47f36d1335dec176b5bbc9deac02e5e0ef50005a
Truong Thanh Tung <ttungbmt@gmail.com>
2022-09-30 16:34:56 +0700

    Update changes

--- src/components/IconButton/IconButton.js
+++ src/components/IconButton/IconButton.js
@@ -2,7 +2,7 @@ import * as React from 'react'
 import { IconButton as MuiIconButton, Menu, MenuItem, ListItemIcon, ListItemText } from '@mui/material'
 import FuseSvgIcon from '../FuseSvgIcon/FuseSvgIcon'
 import Tooltip from '../Tooltip/Tooltip'
-import { isEmpty, pick } from 'lodash'
+import { isArray, isEmpty, pick, reject } from 'lodash'
 import PropTypes from 'prop-types'
 import { useMemo, useState } from 'react'
 
@@ -40,9 +40,18 @@ function ButtonMenu({items, anchorEl, setAnchorEl}){
 }
 
 const IconButton = React.forwardRef((props, ref) => {
-  const { tooltip, color, icon, onClick, children, visible, fontSize, items } = props
+  const { tooltip, color, icon, onClick, children, visible: inVisible, fontSize, items: inItems } = props
   const [anchorEl, setAnchorEl] = useState(null)
-  const shownContent = useMemo(() => visible && icon, [visible, icon])
+
+  const items = useMemo(() => reject(inItems, i => i.visible === false), [inItems])
+
+  const visible = useMemo(() => {
+    if(isArray(inItems)) return !isEmpty(items)
+    return inVisible
+  }, [inVisible, inItems, items])
+
+  const shownContent = useMemo(() => (visible && icon), [visible, icon])
+
 
   const handleClick = (e) => isEmpty(items) ? onClick && onClick(e) : setAnchorEl(e.currentTarget)
 
