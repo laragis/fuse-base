8efeb34b8f0b368468593d46986345f6a30b1412
Truong Thanh Tung <ttungbmt@gmail.com>
2022-11-09 19:40:16 +0700

    Update mobile

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,15 +1,17 @@
-import React, {memo} from 'react'
-import Tippy from '@tippyjs/react';
+import React, { memo } from 'react'
+import Tippy from '@tippyjs/react'
 import './Tooltip.module.css'
+import { isMobile } from 'react-device-detect'
 
-function Tooltip({title, children, ...rest}){
-  if(!title) return children
+function Tooltip({ title, children, ...rest }) {
+  if (!title) return children
 
   const config = {
     theme: 'light',
     animation: 'scale',
     content: title,
     trigger: 'mouseenter',
+    disabled: isMobile,
     ...rest
   }
 
