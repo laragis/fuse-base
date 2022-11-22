4ba7f4d6de8506293f7ee4610dd75ce2b2f01c12
Truong Thanh Tung <ttungbmt@gmail.com>
2022-11-22 14:53:24 +0700

    Revert "Update changes"
    This reverts commit 96ef471177b4af0ccf71561a1c800e292f0cdab2.

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as react from 'react';
-
-declare const Icon: react.ForwardRefExoticComponent<react.RefAttributes<unknown>>;
-
-declare const Tooltip: react.ForwardRefExoticComponent<react.RefAttributes<unknown>>;
-
-declare const IconButton: react.ForwardRefExoticComponent<react.RefAttributes<unknown>>;
-
-export { Icon, IconButton, Tooltip };
--- src/components/Icon/iconClasses.tsx
+++
@@ -1,5 +0,0 @@
-import { unstable_generateUtilityClass as generateUtilityClass } from "@mui/utils";
-
-export function getIconUtilityClass(slot: string): string {
-  return generateUtilityClass('FuseIcon', slot);
-}
